e4caa2f43174a1d15009497b50179317c40e3cfc
Chandru G <102593167+chandrug4@users.noreply.github.com>
2023-10-10 23:21:52 +0530

    Update 0260-single-number-iii.cpp

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-                unordered_map<int,int> mp;
+        // Here the question is using constant extra space but i have used O(n) SC since i learned about hashmap functions and used it here
+        // Once i solve this problem with given constraints i will update the code
+        unordered_map<int,int> mp;
         int size=nums.size();
         vector<int> ans;
         for(int i=0;i<size;i++)
