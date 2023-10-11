cde6b20976321fe364cb0a586d49c9b5f5088483
Chandru G <102593167+chandrug4@users.noreply.github.com>
2023-10-11 18:56:41 +0530

    Time: 101 ms (50.41%), Space: 69.9 MB (39.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,17 +4,29 @@ public:
             unordered_map<int,int> mp;
             for(int i:nums)
             {
+                if(mp[i]>=1) return true;
                 mp[i]++;
             }
 
-            for(auto it:mp)
+            /*for(auto it:mp)
             {
                 if(it.second>1)
                 {
                     return true;
                 }
-            }
+            }*/
             return false;
 
+            /*for(int i=0;i<nums.size();i++)
+            {
+                for(int j=i+1;j<nums.size();j++)
+                {
+                    if(nums[i]==nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;*/
     }
 };
